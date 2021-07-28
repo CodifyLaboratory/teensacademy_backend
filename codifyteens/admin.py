3bf6f206b333cf7c6493a4b813ef2c762089e069
nigoshaya <desm606@gmail.com>
2021-07-28 19:15:21 +0600

    addedadmin

--- codifyteens/admin.py
+++ codifyteens/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+class AboutusAdmin(admin.ModelAdmin):
+    class Meta:
+        model = AboutUs
+
+
+class CourseAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Course
+
+
+models = [AboutUs, Advantages, Course, Contact, Application, Feedback, Mentor, Event, CategoryEvent, FAQ]
+admin.site.register(models)
