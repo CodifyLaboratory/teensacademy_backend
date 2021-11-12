57e9a102e3caab6cf39e28f36cbd3986875c60e0
nigoshaya <desm606@gmail.com>
2021-11-12 20:58:03 +0600

    addedurls

--- codify/urls.py
+++ codify/urls.py
@@ -5,12 +5,15 @@ from django.conf import settings
 from .yasg import urlpatterns as doc_urls
 from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = i18n_patterns(
-    path('admin/', admin.site.urls),
+locale_urlpatterns = i18n_patterns(
     path('api/', include('codifyteens.urls')),
-    path('i18n/', include('django.conf.urls.i18n')),
 )
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('i18n/', include('django.conf.urls.i18n')),
+]
 
+urlpatterns += locale_urlpatterns
 urlpatterns += doc_urls
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
