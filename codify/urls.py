f3edd48fe7555114eef3a5bacbd805051b03e58a
nigoshaya <desm606@gmail.com>
2021-11-12 19:51:26 +0600

    addedi18n

--- codify/urls.py
+++ codify/urls.py
@@ -8,6 +8,7 @@ from django.conf.urls.i18n import i18n_patterns
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('api/', include('codifyteens.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
 )
 
 urlpatterns += doc_urls
